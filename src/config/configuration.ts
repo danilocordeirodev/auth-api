bebefd175c4270e55b6815e37f8691ff7c49a11e
Cordeiro <danilocordeiro.ti@gmail.com>
2024-06-11 21:57:11 +0000

    add config to access database

---
+++ src/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    port:  process.env.PORT ? parseInt(process.env.PORT) :  3000,
+    database: {
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
+      name: process.env.DATABASE_NAME,
+    }
+  });
