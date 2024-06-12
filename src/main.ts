0f7b731505eec930631828430c155d46797f1a3a
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-06-12 01:27:16 -0300

    feat: add base entity

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app';
 import { ConfigService } from '@nestjs/config';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const reflector = app.get(Reflector);
   const configService = app.get(ConfigService);
   const PORT = +configService.get('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
   await app.listen(PORT);
 }
 bootstrap();
